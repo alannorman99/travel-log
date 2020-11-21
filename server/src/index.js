fea1f1d7dcaff2af58e0205c2d916b2f5fc50cfc
Alan Norman <asnorman3604@eagle.fgcu.edu>
2020-11-21 17:38:08 -0500

    Added middleware file to hold error handlers and created readme to lay out the characteristics of the logs

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 
+const middlewares = require('./middlewares')
+
 
 const app = express();
 app.use(morgan('common'));
@@ -17,26 +19,13 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.use((req, res, next) => {
-	const error = new Error(`Not Found - ${req.originalUrl}`);
-	res.status(404);
-	//next forwards to next middleware unless passed an error then it forwards to an error handler
-	next(error);
-});
+app.use(middlewares.notFound);
 
 //error handler
-app.use((error, req, res, next) => {
-	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-	res.status(statusCode);
-	res.json({
-		message: error.message,
-		//process.env.NODE_ENV makes it so stack traces aren't displayed if the app is in production
-		stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
-	});
-});
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 5050;
 
 app.listen(port, () => {
-console.log("Listening on localhost:5050");
+	console.log("Listening on localhost:5050");
 });
